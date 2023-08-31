a1e160fb1a904587262f875fa856e21cc8d13e37
ishifoev <alex.malikov94@gmail.com>
2023-08-31 08:39:14 +0500

    Implement actions for flashcard app list and create

--- app/Console/Commands/FlashcardInteractiveCommand.php
+++ app/Console/Commands/FlashcardInteractiveCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Flashcard;
 use Illuminate\Console\Command;
 
 class FlashcardInteractiveCommand extends Command
@@ -15,35 +16,76 @@ class FlashcardInteractiveCommand extends Command
     {
         $this->info("Welcome to Flashcard Interactive!");
 
-        //Implement main menu and user interactions
-
-        $action = $this->choice("Select an option:", ["Create", "List", "Practice","Stats", "Reset", "Exit"]);
-
-        //Handle different actions
-        switch($action) {
-            case "Create":
-                //Implement flashcard creation logic
-                break;
-            case "List":
-                //Implement flashcard listing logic
-                break;
-            case "Practice":
-                //Implement flashcard practice logic
-                break;
-            case "Stats":
-                //Implement flashcard stats logic
-                break;
-            case "Reset":
-                //Implement flashcard reset logic
-                break;
-            case "Exit":
-                $this->info("Exiting Flashcard Interactive");
-                return;
-            default:
-               $this->error("Invalid action selected");
-               break;
-        }
         
-        $this->line("Thank you for using Flashcard interactive!");
+
+        while(true) {
+            //Implement main menu and user interactions
+            $action = $this->choice("Select an option:", ["Create", "List", "Practice","Stats", "Reset", "Exit"]);
+
+            //Handle different actions
+            switch($action) {
+                case "Create":
+                    //Implement flashcard creation logic
+                    $this->createFlashCard();
+                    break;
+                case "List":
+                    //Implement flashcard listing logic
+                    $this->listFlashcards();
+                    break;
+                case "Practice":
+                    //Implement flashcard practice logic
+                    $this->practiceFlashcards();
+                    break;
+                case "Stats":
+                    //Implement flashcard stats logic
+                    $this->displayStats();
+                    break;
+                case "Reset":
+                    $this->resetProgress();
+                    //Implement flashcard reset logic
+                    break;
+                case "Exit":
+                    $this->info("Exiting Flashcard Interactive");
+                    return;
+                default:
+                $this->error("Invalid action selected");
+                break;
+            }
+        }
+    }
+
+    private function createFlashCard()
+    {
+        $question = $this->ask("Enter the flashcard question:");
+        $answer = $this->ask("Enter the flashcard answer:");
+
+        Flashcard::create([
+            "question" => $question,
+            "answer" => $answer
+        ]);
+        
+        $this->info("Flashcard created successfully.");
+    }
+
+    private function listFlashcards() 
+    {
+        $flashcards = Flashcard::all();
+
+        $this->table(["ID", "Question", "Answer"], $flashcards);
+    }
+
+    private function practiceFlashcards() 
+    {
+        //Implement flashcard practicing logic
+    }
+
+    private function displayStats()
+    {
+        //Implement flashcard statistics logic
+    }
+
+    private function resetProgress()
+    {
+        //Implement flashcard reset logic
     }
 }
